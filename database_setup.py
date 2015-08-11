c794008843811ae07c9ec91247304eab066f3e0e
Juan Camilo <juancgr_4@hotmail.com>
2015-08-11 01:12:44 -0500

    JSON modification

--- database_setup.py
+++ database_setup.py
@@ -1,3 +1,5 @@
+# Database built on sqlalchemy that holds Items and Categories related to this items
+# Developed by : Juan Camilo Gutierrez Ruiz - 20/July/2015
 import os
 import sys
 from sqlalchemy import Column, Integer, ForeignKey, String
@@ -7,13 +9,15 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+#Table for the categories
 class CategoryTable(Base):
 	__tablename__= 'Category'
 
 	id = Column(Integer, primary_key=True)
 	name = Column(String(250), nullable=False)
-
+# Table for the items
 class ItemTable(Base):
+
 	__tablename__ = 'Item'
 
 	id = Column(Integer, primary_key=True)
@@ -22,6 +26,13 @@ class ItemTable(Base):
 	image = Column(String(250), nullable=False)
 	category_id =Column(Integer, ForeignKey('Category.id'))
 	category = relationship(CategoryTable)
+	# Property which later will be used in the jsonify view.
+	@property
+	def serial(self):
+		return{
+		'name':self.name,
+		'description':self.description,
+		}
 
 engine = create_engine('sqlite:///ItemCatalog.db')
 Base.metadata.create_all(engine)
